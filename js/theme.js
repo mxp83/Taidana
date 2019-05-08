f5c521251f0dd95841042ac76be0ab4ee04ce26b
Unknown <marc.payumo@gmail.com>
2019-05-08 16:13:00 -0700

    Adding a footer dynamically through wordpress
    removed the php content in footer.php

--- js/theme.js
+++ js/theme.js
@@ -7054,3 +7054,19 @@ $(document).ready(function(){
 	$('body').toggleClass('nav-open');
   });
 });
+
+// dropdown hover instead of toggle
+
+$(".dropdown-toggle").on("mouseenter", function () {
+    // make sure it is not shown:
+    if (!$(this).parent().hasClass("show")) {
+        $(this).click();
+    }
+});
+
+$(".btn-group, .dropdown").on("mouseleave", function () {
+    // make sure it is shown:
+    if ($(this).hasClass("show")){
+        $(this).children('.dropdown-toggle').first().click();
+    }
+});
--- loop-templates/content-page.php
+++ loop-templates/content-page.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<header class="entry-header">
 
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<!-- <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?> -->
 
 	</header><!-- .entry-header -->
 
